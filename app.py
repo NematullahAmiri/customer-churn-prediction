b649af94d1ec5bae0c6e1f1fb2598712b9ff4c75
Nematullah Amiri <nemat_2929@yahoo.com>
2026-06-16 14:25:52 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,15 @@
+import streamlit as st
+import pandas as pd
+import joblib
 
-PASTE THE FINAL STREAMLIT CODE HERE
+model = joblib.load("customer_churn_model.pkl")
+
+st.set_page_config(
+    page_title="Customer Churn Prediction",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("📊 Customer Churn Prediction System")
+
+st.write("Predict whether a customer is likely to churn.")
